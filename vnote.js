02c97700cc0001f36d50ce9506a8299bb65a59f9
Mr-rabbit-xd <bihh5539@gmail.com>
2026-01-19 09:16:18 +0530

    Update vnote.js

--- vnote.js
+++ vnote.js
@@ -3,14 +3,13 @@ import fs from "fs";
 
 export async function sendVnote(sock, number, url) {
   const res = await axios.get(url, { responseType: "arraybuffer" });
-
   const file = `./${Date.now()}.mp3`;
   fs.writeFileSync(file, res.data);
 
   await sock.sendMessage(number + "@s.whatsapp.net", {
     audio: fs.readFileSync(file),
     mimetype: "audio/mpeg",
-    ptt: true // 🔥 VOICE NOTE
+    ptt: true
   });
 
   fs.unlinkSync(file);
