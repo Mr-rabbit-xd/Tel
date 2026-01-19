64e8fb593a6e94fd35648a32d1a9be711336319c
Mr-rabbit-xd <bihh5539@gmail.com>
2026-01-19 08:59:33 +0530

    Create vnote.js

---
+++ vnote.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import fs from "fs";
+
+export async function sendVnote(sock, number, url) {
+  const res = await axios.get(url, { responseType: "arraybuffer" });
+
+  const file = `./${Date.now()}.mp3`;
+  fs.writeFileSync(file, res.data);
+
+  await sock.sendMessage(number + "@s.whatsapp.net", {
+    audio: fs.readFileSync(file),
+    mimetype: "audio/mpeg",
+    ptt: true // 🔥 VOICE NOTE
+  });
+
+  fs.unlinkSync(file);
+}
